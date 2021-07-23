dac89d3b66d4503a86facfae972afe1fb26c8120
markoraguz6 <raguzmarko18@gmail.com>
2021-07-23 15:14:01 +0200

    Angular-CRUD

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+
+// Post method
+  postEmployee(data : any){
+    return this.http.post<any>("http://localhost:3000/posts", data)
+    .pipe(map((res:any) => {
+      return res;
+    }))
+}
+
+// Get method
+getEmployee(){
+  return this.http.get<any>("http://localhost:3000/posts")
+  .pipe(map((res:any) => {
+    return res;
+  }))
+}
+
+// Update method
+updateEmployee(data : any, id : number){
+  return this.http.put<any>("http://localhost:3000/posts/"+id, data)
+  .pipe(map((res:any) => {
+    return res;
+  }))
+} 
+
+//Delete method
+deleteEmployee(id : number){
+  return this.http.delete<any>("http://localhost:3000/posts/"+id)
+  .pipe(map((res:any) => {
+    return res;
+  }))
+}
+}
